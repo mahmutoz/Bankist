3a984e1c6e0b4d3d76474397a84aefe55642bfb3
= <=>
2021-05-10 22:14:50 +0300

    unnecessary codes removed

--- script.js
+++ script.js
@@ -191,12 +191,3 @@ btnClose.addEventListener('click', (e) => {
 
   inputCloseUsername.value = inputClosePin.value = '';
 });
-
-// LECTURES
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
